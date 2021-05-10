0ddb4956d5894e60d0677a38fbabd2d92279f8ac
GaGandour <64374553+GaGandour@users.noreply.github.com>
2021-05-10 19:55:02 -0300

    agoravai?

--- lib/adHelper.dart
+++ lib/adHelper.dart
@@ -3,9 +3,9 @@ import 'dart:io';
 class AdHelperInicio {
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-6358831879798651/1999339508';
+      return 'ca-app-pub-3940256099942544/6300978111'; //teste
     } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/2934735716';
+      return 'ca-app-pub-3940256099942544/2934735716'; //teste
     } else {
       throw new UnsupportedError('Unsupported platform');
     }
@@ -15,9 +15,9 @@ class AdHelperInicio {
 class AdHelperMenu {
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-6358831879798651/9985640732';
+      return 'ca-app-pub-3940256099942544/6300978111'; //teste
     } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/2934735716';
+      return 'ca-app-pub-3940256099942544/2934735716'; //teste
     } else {
       throw new UnsupportedError('Unsupported platform');
     }
