21e3d25f9ac804cefe6b952010fbbfa2f5a54c2b
GaGandour <64374553+GaGandour@users.noreply.github.com>
2021-05-16 11:49:07 -0300

    comentzinho pra ajudar
    dado do ID de banner de teste

--- lib/adHelper.dart
+++ lib/adHelper.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 class AdHelperInicio {
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/6300978111'; //teste
+      return 'ca-app-pub-3940256099942544/6300978111'; //teste: ca-app-pub-3940256099942544/6300978111
     } else if (Platform.isIOS) {
       return 'ca-app-pub-3940256099942544/2934735716'; //teste
     } else {
@@ -15,7 +15,7 @@ class AdHelperInicio {
 class AdHelperMenu {
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/6300978111'; //teste
+      return 'ca-app-pub-3940256099942544/6300978111'; //teste: ca-app-pub-3940256099942544/6300978111
     } else if (Platform.isIOS) {
       return 'ca-app-pub-3940256099942544/2934735716'; //teste
     } else {
